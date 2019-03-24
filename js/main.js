7851e5d8604d4c5d9fd6e17cadf826876424b930
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-03-25 00:26:22 +0100

    Final Version

--- js/main.js
+++ js/main.js
@@ -1,31 +1,20 @@
 'use strict';
-var iphone6S = new Phone('Apple', 'ios', 2250, 'silver');
-var samsungGalaxyS6 = new Phone('Samsung', 'android', 1850, 'black');
-var onePlusOne = new Phone('OnePlus', 'android', 1700, 'gold');
-var getWarrantyCost = new Cost(225, 185, 170);
+var iphone6S = new Phone('Apple', 'ios', 2250, 'silver', 0.1);
+var samsungGalaxyS6 = new Phone('Samsung', 'android', 1850, 'black', 0.1);
+var onePlusOne = new Phone('OnePlus', 'android', 1700, 'gold', 0.1);
 
-function Phone(brand, system, price, color) {
+function Phone(brand, system, price, color, getWarrantyCost) {
     this.brand = brand;
     this.price = price;
     this.color = color;
     this.system = system;
-}
-
-function Cost(appleCost, samsungCost, oneplusCost) {
-    this.apple = appleCost;
-    this.samsung = samsungCost;
-    this.oneplus = oneplusCost;
+    this.warranty = price * getWarrantyCost;
 }
 
 Phone.prototype.printInfo = function () {
-    console.log('The phone brand is ' + this.brand + ' with system ' + this.system + ', color is ' + this.color + ' and the price is ' + this.price + '.');
-};
-
-Cost.prototype.printInfo = function () {
-    console.log('Exclusive Warranty for Apple Phones cost is ' + this.apple + ' for samsung you pay only ' + this.samsung + ' and for OnePlus Warranty costb per YEAR is ' + this.oneplus + '.');
+    console.log('The phone brand is ' + this.brand + ' with system ' + this.system + ', color is ' + this.color + ' and the price is ' + this.price + '.' + 'We have premium Warranty for every model cost for ' + this.brand + ' is ' + this.warranty + '.');
 };
 
 iphone6S.printInfo();
 samsungGalaxyS6.printInfo();
 onePlusOne.printInfo();
-getWarrantyCost.printInfo();
