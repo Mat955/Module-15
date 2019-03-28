03cb412a563258e29828bfb6a2e78b41ce7dc325
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-03-28 21:42:20 +0100

    warranty v2

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 'use strict';
-var iphone6S = new Phone('Apple', 'ios', 2250, 'silver', 0.1);
-var samsungGalaxyS6 = new Phone('Samsung', 'android', 1850, 'black', 0.1);
-var onePlusOne = new Phone('OnePlus', 'android', 1700, 'gold', 0.1);
+var iphone6S = new Phone('Apple', 'ios', 2250, 'silver');
+var samsungGalaxyS6 = new Phone('Samsung', 'android', 1850, 'black');
+var onePlusOne = new Phone('OnePlus', 'android', 1700, 'gold');
 
-function Phone(brand, system, price, color, getWarrantyCost) {
+function Phone(brand, system, price, color) {
     this.brand = brand;
     this.price = price;
     this.color = color;
     this.system = system;
-    this.warranty = price * getWarrantyCost;
+    this.getWarrantyCost = this.price * 0.1;
 }
 
 Phone.prototype.printInfo = function () {
-    console.log('The phone brand is ' + this.brand + ' with system ' + this.system + ', color is ' + this.color + ' and the price is ' + this.price + '.' + 'We have premium Warranty for every model cost for ' + this.brand + ' is ' + this.warranty + '.');
+    console.log('The phone brand is ' + this.brand + ' with system ' + this.system + ', color is ' + this.color + ' and the price is ' + this.price + '.' + ' We offer you 5 years warranty for only: ' + this.getWarrantyCost + '.');
 };
 
 iphone6S.printInfo();
