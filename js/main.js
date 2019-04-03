551da4d4686f141406fd10d93e06f03f71b15935
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-04-03 20:12:41 +0200

    final version

--- js/main.js
+++ js/main.js
@@ -8,12 +8,16 @@ function Phone(brand, system, price, color) {
     this.price = price;
     this.color = color;
     this.system = system;
-    this.getWarrantyCost = this.price * 0.1;
+}
+
+Phone.prototype.getWarrantyCost = function() {
+    return this.price * 0.1;
 }
 
 Phone.prototype.printInfo = function () {
-    console.log('The phone brand is ' + this.brand + ' with system ' + this.system + ', color is ' + this.color + ' and the price is ' + this.price + '.' + ' We offer you 5 years warranty for only: ' + this.getWarrantyCost + '.');
-};
+    console.log('The phone brand is ' + this.brand + ' with system ' + this.system + ', color is ' + this.color + ' and the price is ' + this.price + '.');
+    console.log('We offer you 5 years warranty for only: ' + this.getWarrantyCost());
+ };
 
 iphone6S.printInfo();
 samsungGalaxyS6.printInfo();
